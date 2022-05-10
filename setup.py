02eb54d75d1966f6b7e42f8683db9130ef553447
Junyao Wang <junwow@amazon.com>
2022-05-10 00:16:24 -0700

    update setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,11 @@ from setuptools import setup, find_packages
 with open('LICENSE') as f:
     license = f.read()
 
+
+with open("requirements.txt") as f:
+    requireds = f.read().splitlines()
+
+
 setup(
     name='NftRarity',
     version='0.1.0',
@@ -11,6 +16,7 @@ setup(
     author='JJ',
     author_email='',
     url='https://github.com/jj-btc/NftRarity',
+    install_requires=requireds,
     license=license,
     packages=find_packages(exclude=('tests', 'docs'))
 )
